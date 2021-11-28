a7c0a675e53a4ed8d6981f339915b32dc4169ef5
Muna Dahir <muna.dahir@hotmail.com>
2021-11-28 17:09:29 -0500

    added bcryptjs, cookie-session and helper function file

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 require("dotenv").config();
 
 // Web server config
-const PORT = process.env.PORT || 8080;
-const sassMiddleware = require("./lib/sass-middleware");
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-
+const PORT             = process.env.PORT || 8080;
+const sassMiddleware   = require("./lib/sass-middleware");
+const express          = require("express");
+const app              = express();
+const morgan           = require("morgan");
+const bcrypt           = require('bcryptjs');
+const cookieSession    = require('cookie-session');
 // PG database client/connection setup
 const { Pool } = require("pg");
 const dbParams = require("./lib/db.js");
@@ -18,6 +19,10 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
+app.use(cookieSession({
+  name: 'session',
+  keys: [proccess.env.COOKIE_KEY]
+}));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
