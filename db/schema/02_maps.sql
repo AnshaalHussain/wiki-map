4dc0360faebeb2db679e3eba888d93ebf29bbcf9
Muna Dahir <muna.dahir@hotmail.com>
2021-11-29 17:22:01 -0500

    changed the data type of favourtite

--- db/schema/02_maps.sql
+++ db/schema/02_maps.sql
@@ -4,7 +4,7 @@ CREATE TABLE maps (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
   description VARCHAR(255) NOT NULL,
-  favourite VARCHAR(255) NOT NULL,
+  favourite BOOLEAN,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
