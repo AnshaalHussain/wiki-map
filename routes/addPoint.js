74fd09e68c1db5080d7f867ba436bbbe1d7505c0
Muna Dahir <muna.dahir@hotmail.com>
2021-12-02 18:22:37 -0500

    aded point route

---
+++ routes/addPoint.js
@@ -0,0 +1,35 @@
+const express          = require('express');
+const router           = express.Router();
+const helpers = require('../lib/map_helpers');
+const cookieSession    = require('cookie-session');
+const bodyparser = require('body-parser')
+
+
+router.use(bodyparser.json());
+router.use(cookieSession({
+  name: 'session',
+  keys: [process.env.COOKIE_KEY]
+}));
+
+
+module.exports = (db) => {
+
+  // POST /addpoints
+ // saves a point after a form is submitted
+ router.post("/", (req,res) => {
+
+  const point = req.body
+  const userId = req.session.userId;
+  const pointVal = {...point, user_id: userId, map_id: 19 };
+  console.log(pointVal)
+  //need to add the point data into the function
+  helpers.addPoint(pointVal)
+    .then(point => {
+      console.log("inside post request --- point added", point)
+        res.json(point);
+
+    })
+    .catch(err => res.status(404).send(err));
+  });
+  return router;
+};
